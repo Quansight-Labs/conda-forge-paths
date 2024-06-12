0a653b99bbecfee939406abdcc56344276b4f408
jaimergp <jaimergp@users.noreply.github.com>
2024-06-12 09:22:17 +0200

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-set -euxo pipefail
-
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate datasette
+
+set -euxo pipefail
 
 if [[ $1 == "update" ]]; then
     curl -sfL -o path_to_artifacts.tar.zst \
         https://github.com/jaimergp/conda-forge-paths/releases/latest/download/path_to_artifacts.tar.zst
-    curl -L -o path_to_artifacts.db.sha256 \
+    curl -sfL -o path_to_artifacts.db.sha256 \
         https://github.com/jaimergp/conda-forge-paths/releases/latest/download/path_to_artifacts.db.sha256
     mkdir -p extracted
     tar xf path_to_artifacts.tar.zst -C extracted
     rm path_to_artifacts.tar.zst
 
-    if [[ "$(openssl sha256 path_to_artifacts.db)" != "$(cat path_to_artifacts.db.sha256)" ]]; then
+    if [[ "$(openssl sha256 extracted/path_to_artifacts.db | cut -d ' ' -f2)" != "$(cat path_to_artifacts.db.sha256  | cut -d ' ' -f2)" ]]; then
         echo "SHA256 mismatch! Won't update redeploy"
         exit 1
     fi
     mv extracted/path_to_artifacts.db path_to_artifacts.db
 
     # curl -sfL -o datasette.update.yml \
-    #     https://raw.githubusercontent.com/jaimergp/conda-forge-paths/main/datasette.yml \
+    #     https://github.com/jaimergp/conda-forge-paths/releases/latest/download/datasette.yml \
     #     && mv datasette.update.yml datasette.yml \
     #     || true
 elif [[ $1 == "run" ]]; then
